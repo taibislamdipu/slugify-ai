ac336fbc586532787b2eba02792d9d5705f31911
taibislamdipu <mailtaibislam@gmail.com>
2024-08-30 12:56:24 +0600

    on progress

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content={metadata.description} />
         <meta
           property="og:image"
-          content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/slugifyai.vercel.app/World's%20First%20AI-Powered%20Slug%20Generator%20-%20Optimize%20Your%20URLs%20with%20Smart%20Technology/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F0a4e6d7f-88c7-4ee5-9098-0accd65973e9.jpg%3Ftoken%3D_zO2qxG5S418RV5FCWqDd399IncVzg1LDHfwVcSpJcM%26height%3D600%26width%3D400%26expires%3D33260999677/og.png"
+          content="https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/slugifyai.vercel.app/World's%20First%20AI-Powered%20Slug%20Generator%20-%20Optimize%20Your%20URLs%20with%20Smart%20Technology/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F65479a96-df81-49a0-9711-227cb5f5d059.jpg%3Ftoken%3Dzvh25gZWKF9blxwagRXb3SljiIRy7Axljpnxpy8zIc8%26height%3D900%26width%3D1200%26expires%3D33260996414/og.png"
         />
 
         {/* Twitter Meta Tags */}
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
         <meta name="twitter:description" content={metadata.description} />
         <meta
           name="twitter:image"
-          content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/slugifyai.vercel.app/World's%20First%20AI-Powered%20Slug%20Generator%20-%20Optimize%20Your%20URLs%20with%20Smart%20Technology/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F0a4e6d7f-88c7-4ee5-9098-0accd65973e9.jpg%3Ftoken%3D_zO2qxG5S418RV5FCWqDd399IncVzg1LDHfwVcSpJcM%26height%3D600%26width%3D400%26expires%3D33260999677/og.png"
+          content="https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/slugifyai.vercel.app/World's%20First%20AI-Powered%20Slug%20Generator%20-%20Optimize%20Your%20URLs%20with%20Smart%20Technology/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F65479a96-df81-49a0-9711-227cb5f5d059.jpg%3Ftoken%3Dzvh25gZWKF9blxwagRXb3SljiIRy7Axljpnxpy8zIc8%26height%3D900%26width%3D1200%26expires%3D33260996414/og.png"
         />
       </head>
       <body className={raleway.className}>
